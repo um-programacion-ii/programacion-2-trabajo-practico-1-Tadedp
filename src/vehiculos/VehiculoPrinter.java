88862e90524c1da48e1308680eea0208f1c9f3fa
Tadedp <tadeodp22@gmail.com>
2025-04-06 23:26:06 -0300

    Polimorfismo para imprimir camiones en VehiculoPrinter implementado.

--- src/vehiculos/VehiculoPrinter.java
+++ src/vehiculos/VehiculoPrinter.java
@@ -4,10 +4,18 @@ public class VehiculoPrinter {
     public VehiculoPrinter() {
     }
 
-    public void mostrarInformacion(Vehiculo vehiculo){
+    public void mostrarInformacion(Vehiculo vehiculo) {
         System.out.println("- Patente: " + vehiculo.getPatente() + "\n" +
                 "- Marca: " + vehiculo.getMarca() + "\n" +
                 "- Año: " + vehiculo.getAnio() + "\n" +
                 "- Capacidad de carga: " + vehiculo.getCapacidadCargaKg() + " kg.");
+
+        if (vehiculo instanceof Camion) {
+            if (((Camion) vehiculo).getTieneAcoplado()) {
+                System.out.println("- Tiene acoplado");
+            } else {
+                System.out.println("- No tiene acoplado");
+            }
+        }
     }
 }
